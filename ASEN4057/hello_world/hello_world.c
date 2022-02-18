39b4693c1e8eb40c2d00146ca1a84fd6b3c6e46a
Dominic Dougherty <dodo1536@asen4057.int.colorado.edu>
2022-02-18 05:11:28 +0000

    Directory Structure

---
+++ ASEN4057/hello_world/hello_world.c
@@ -0,0 +1,17 @@
+/*
+*
+* File: hello_world.c
+* Author: Dominic Dougherty
+*/
+
+/* Include preprocessor directives*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main( int argc, char** argv)
+{
+	// print hello world
+	printf(" Hello World!\n");
+	return (EXIT_SUCCESS);
+}
+
